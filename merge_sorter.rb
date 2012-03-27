34b9cd5c357653eb9fe781e6e030db74f99bdc3c
Anjali Shenoy <anjshenoy@gmail.com>
2012-03-27 11:08:19 -0700

    Added ability to count number of inversions

--- merge_sorter.rb
+++ merge_sorter.rb
@@ -1,18 +1,41 @@
 class MergeSorter
 
-  def self.divide(array)
-    puts "input = #{array.inspect}"
+  #also counts number of inversions
+  # an inversion is defined as followed
+  # A pair of elements at index positions i,j 
+  # such that i<j and A[i] > A[j]
+  # The brute force way of getting such inversions
+  # would be to have 2 for loops the outer one with
+  # i=0..n and the inner one with j=i+i..n
+  # but that would yield a time complexity of n^2.
+  # Instead we can piggyback off of merge sort and 
+  # count the number of inversions when doing the
+  # merge step, specifically when comparing the 
+  # elements from the subarrays. When the element
+  # in the second subarray is less than the element 
+  # in the first, we have an inversion. Actually,
+  # in such a scenario, we have as many inversions
+  # as there are elements remaining in the first
+  # array.
+
+  attr_accessor :input, :inversions
+  def initialize()
+    self.inversions = []
+  end
+
+  def divide(array)
+    #puts "input = #{array.inspect}"
     return array if array.size == 1
     m = ((array.size-1)/2).floor
     output = merge(divide(array[0..m]), divide(array[m+1...array.size]))
-    puts "merge result = #{output.inspect}"
+    #puts "merge result = #{output.inspect}"
     return output
   end
 
 
   private
-  def self.merge(arr1, arr2)
-    puts "calling merge with #{arr1.inspect} #{arr2.inspect}"
+  def merge(arr1, arr2)
+#    puts "calling merge with #{arr1.inspect} #{arr2.inspect}"
     i,j = 0, 0
     output = []
     while(i < arr1.size && j < arr2.size) do
@@ -21,6 +44,7 @@ class MergeSorter
         i += 1
       else
         output << arr2[j]
+        arr1[i...arr1.size].each{|x| inversions << [x, arr2[j]]}
         j += 1
       end
     end
@@ -36,4 +60,7 @@ end
 
 
 a= [5,15, 2, 9, 8, 7]
-puts MergeSorter.divide(a)
+m = MergeSorter.new
+puts "sorted array = #{m.divide(a)}"
+puts "*"*100
+puts "inversions = #{m.inversions}"
